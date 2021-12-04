6a5a4003a54c8ac1336e6373f8404ef46f052705
abigeater <abigeater@163.com>
2021-12-04 17:37:56 +0800

    update 替换底层Connection 在查询事件结束时执行事件将$statement传递出去使用 该做法应能解决查询之后不能执行存储过程 & 方便转Json行为

--- config/autoload/annotations.php
+++ config/autoload/annotations.php
@@ -9,6 +9,9 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
+
+use Hyperf\Database\Connection;
+
 return [
     'scan' => [
         'paths' => [
@@ -16,6 +19,9 @@ return [
         ],
         'ignore_annotations' => [
             'mixin',
+        ],
+        'class_map' => [
+            Connection::class => BASE_PATH . '/class_map/Hyperf/Database/Connection.php',
         ],
     ],
 ];
