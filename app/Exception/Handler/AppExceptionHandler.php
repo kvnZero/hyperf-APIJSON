50a1421a4c869a4e5fc5377368cf1b20b599c215
marin <marunrun@163.com>
2022-04-22 13:54:44 +0800

    feat : 增加异常打印至控制台

--- app/Exception/Handler/AppExceptionHandler.php
+++ app/Exception/Handler/AppExceptionHandler.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
 namespace App\Exception\Handler;
 
 use App\Constants\ResponseCode;
+use Hyperf\Contract\StdoutLoggerInterface;
 use Hyperf\ExceptionHandler\ExceptionHandler;
 use Hyperf\HttpMessage\Stream\SwooleStream;
 use Psr\Http\Message\ResponseInterface;
@@ -19,8 +20,21 @@ use Throwable;
 
 class AppExceptionHandler extends ExceptionHandler
 {
+
+    /**
+     * @var StdoutLoggerInterface
+     */
+    protected StdoutLoggerInterface $logger;
+
+    public function __construct(StdoutLoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function handle(Throwable $throwable, ResponseInterface $response)
     {
+        $this->logger->error(sprintf('%s[%s] in %s', $throwable->getMessage(), $throwable->getLine(), $throwable->getFile()));
+        $this->logger->error($throwable->getTraceAsString());
         return $response->withHeader('Server', 'Hyperf')->withHeader('Content-Type', 'application/json')->withStatus(500)->withBody(new SwooleStream(json_encode([
             'code' => $throwable->getCode() ? $throwable->getCode() : ResponseCode::CODE_SERVER_ERROR,
             'msg' =>  $throwable->getCode() ? $throwable->getMessage() : ResponseCode::getMessage(ResponseCode::CODE_SERVER_ERROR)
