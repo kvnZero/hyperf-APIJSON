b1cf719acb976f19b0321908a798e9821ec3f425
marin <marunrun@163.com>
2022-04-22 13:54:19 +0800

    fix : 修改默认DB_DRIVER 为 mysql
    修改请求未带origin header头时的一个小问题

--- config/autoload/databases.php
+++ config/autoload/databases.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  */
 return [
     'default' => [
-        'driver' => env('DB_DRIVER', 'pdo'),
+        'driver' => env('DB_DRIVER', 'mysql'),
         'host' => env('DB_HOST', 'localhost'),
         'database' => env('DB_DATABASE', 'hyperf'),
         'port' => env('DB_PORT', 3306),
